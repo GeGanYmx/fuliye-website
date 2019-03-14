fe3195e19589f476d0ba42863d1c43f0113f04ca
GeGanYmx <2319649532@qq.com>
2019-03-14 16:35:50 +0800

    add js login

---
+++ src/global.js
@@ -0,0 +1,9 @@
+`use strict`;
+var Global={
+    register:function(){
+        window.location.href='register.html';
+    },
+    login:function(){
+        window.location.href='login.html';
+    }
+}
--- src/home.js
+++
@@ -1,9 +0,0 @@
-`use strict`
-var homejs={
-    loginIn:function(){
-        alert('点击登录');
-    },
-    signUp:function(){
-        alert(`点击注册`);
-    }
-}
