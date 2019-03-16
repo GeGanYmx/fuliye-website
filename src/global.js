804a4e6a93db84285c597ce3c3864d22d15eb991
GeGanYmx <2319649532@qq.com>
2019-03-16 17:37:27 +0800

    modify project directory

--- src/global.js
+++
@@ -1,9 +0,0 @@
-`use strict`;
-var Global={
-    register:function(){
-        window.location.href='register.html';
-    },
-    login:function(){
-        window.location.href='login.html';
-    }
-}
---
+++ src/js/global.js
@@ -0,0 +1,9 @@
+`use strict`;
+var Global={
+    register:function(){
+        window.location.href='../pages/register.html';
+    },
+    login:function(){
+        window.location.href='../pages/login.html';
+    }
+}
