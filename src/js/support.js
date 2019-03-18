5631068b38413e6bdc1479e66f10226cd1b1d13f
GeGanYmx <2319649532@qq.com>
2019-03-18 17:25:52 +0800

    update support

--- src/js/support.js
+++ src/js/support.js
@@ -12,22 +12,34 @@ var support={
                 $(e).attr('src','./assets/content_icon2_elected.png');
                 $(e).parent().children('span').css('color','#0086D1');
                 $(e).parent().parent().removeClass('question-answer').addClass('question-answer-active');
-                /*$(e).parent().parent().css('background-color','#FFF');*/
-                /*$(e).parent().removeClass('article-question').addClass('article-question-active');*/
             }else{
                 answer.slideUp('slow');
                 $(e).attr('src','./assets/content_icon2_default.png');
                 $(e).parent().children('span').css('color','#606470');
                 $(e).parent().parent().removeClass('question-answer-active').addClass('question-answer');
-                /*$(e).parent().parent().css('background-color','rgba(255,255,255,0.20)');*/
-                /*$(e).parent().removeClass('article-question-active').addClass('article-question');*/
+            }
+        });
+    },
+    toggleAnswer2:function(e){
+        $(function(){
+            var answer=$(e).parent().parent().find('div.article-answer');
+            console.log(answer.css('display'));
+            if(answer.css('display')==='none'){
+                answer.slideDown('slow');
+                $(e).attr('src','./assets/content_icon2_elected.png');
+                $(e).parent().children('span').css('color','#0086D1');
+                $(e).parent().parent().removeClass('question-answer').addClass('question-answer-active2');
+            }else{
+                answer.slideUp('slow');
+                $(e).attr('src','./assets/content_icon2_default.png');
+                $(e).parent().children('span').css('color','#606470');
+                $(e).parent().parent().removeClass('question-answer-active2').addClass('question-answer');
             }
         });
     },
     toggleTab:function(e){
        console.log($(e));
        $(e).find('span.icon-span').css('color',' #0A1017');
-       /*console.log($(e).siblings('li').find('span.icon-span'));*/
        $(e).siblings('li').find('span.icon-span').css('color','#999999');
 }
 }
