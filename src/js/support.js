937d7ca7efbd8205fe72825bc6ae0e1ea66522f9
GeGanYmx <2319649532@qq.com>
2019-03-19 10:57:19 +0800

    update support

--- src/js/support.js
+++ src/js/support.js
@@ -20,23 +20,6 @@ var support={
             }
         });
     },
-    toggleAnswer2:function(e){
-        $(function(){
-            var answer=$(e).parent().parent().find('div.article-answer');
-            console.log(answer.css('display'));
-            if(answer.css('display')==='none'){
-                answer.slideDown('slow');
-                $(e).attr('src','./assets/content_icon2_elected.png');
-                $(e).parent().children('span').css('color','#0086D1');
-                $(e).parent().parent().removeClass('question-answer').addClass('question-answer-active2');
-            }else{
-                answer.slideUp('slow');
-                $(e).attr('src','./assets/content_icon2_default.png');
-                $(e).parent().children('span').css('color','#606470');
-                $(e).parent().parent().removeClass('question-answer-active2').addClass('question-answer');
-            }
-        });
-    },
     toggleTab:function(e){
        console.log($(e));
        $(e).find('span.icon-span').css('color',' #0A1017');
