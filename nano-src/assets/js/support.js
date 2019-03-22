f926a59721b148e603284186e6eb19aa8a887825
孙达 <sunda@sundaMBA.local>
2019-03-22 17:29:33 +0800

    update support pic

--- nano-src/assets/js/support.js
+++ nano-src/assets/js/support.js
@@ -9,13 +9,13 @@ var support={
             console.log(answer.css('display'));
             if(answer.css('display')==='none'){
                 answer.slideDown('slow');
-                $(e).attr('src','../assets/up.png');
+                $(e).attr('src','./assets/up.png');
                 $(e).parent().children('span').css('color','#0086D1');
                 /*$(e).parent().parent().css('background-color','#FFF');*/
                 /*$(e).parent().removeClass('article-question').addClass('article-question-active');*/
             }else{
                 answer.slideUp('slow');
-                $(e).attr('src','../assets/down.png');
+                $(e).attr('src','./assets/down.png');
                 $(e).parent().children('span').css('color','#606470');
                 /*$(e).parent().parent().css('background-color','rgba(255,255,255,0.20)');*/
                 /*$(e).parent().removeClass('article-question-active').addClass('article-question');*/
